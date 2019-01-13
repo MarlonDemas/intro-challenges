14e425e3bea4ea3e04487f339c198faf2db84594
MarlonDemas <demasmarlon@gmail.com>
2019-01-13 14:17:22 +0200

    created new folder and index file for form challenge with base code and main heading with page title

---
+++ php form challenge/index.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>PHP Form Challenge</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.css"/>
+ </head>
+ <body>
+    <h1 class="title is-1 has-text-centered">PHP Form Challenge</h1>
+    <hr>     
+ </body>
+ </html>
